75fd8287ae37f0999fbf4482c297b1a9078e87f1
RENgOLD <rengold@me.com>
2026-04-14 16:42:08 +0400

    Урок 1.6.2

---
+++ 1/1-6-2.py
@@ -0,0 +1,12 @@
+from os import error
+from sys import exception
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/simple_form_find_task.html")
+    button = browser.find_element(By.ID, "submit_button")
+except:
+    print(exception())
