634e7479897de43d6771370e6fd1c3fc2dba2aee
RENgOLD <rengold@me.com>
2026-04-17 12:04:10 +0400

    Урок 2.2.7

---
+++ 2/2-2-7.py
@@ -0,0 +1,9 @@
+# https://stepik.org/lesson/228249/step/7?unit=200781
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from pathlib import Path
+
+filePath = Path('2-2-6.py')
+print(filePath.absolute())
