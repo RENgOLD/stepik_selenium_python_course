36549c99d5cc96b1142ed7a9a055b7330f328533
RENgOLD <rengold@me.com>
2026-04-26 11:19:01 +0400

    Урок 3.6.7

--- 3/conftest.py
+++ 3/conftest.py
@@ -3,6 +3,10 @@ from pathlib import Path
 import pytest
 import json
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+
 @pytest.fixture
 def stepik_creds():
     path = Path('creds.json')
@@ -10,9 +14,17 @@ def stepik_creds():
     return creds['login'], creds['password']
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nStart chrome browser for test...")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nStart firefox browser for test...")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print("\nquit browser..")
+    print("\nQuit browser...")
     browser.quit()
